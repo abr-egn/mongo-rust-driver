cdc219fefa030ec8656f593a12f1bb2ffefdbeb9
Abraham Egnor <abraham.egnor@mongodb.com>
2026-02-10 10:35:29 +0000

    copy docs

--- driver/src/cursor/two.rs
+++ driver/src/cursor/two.rs
@@ -1,5 +1,3 @@
-#![allow(missing_docs)]
-
 mod common;
 pub(crate) mod session;
 #[cfg(feature = "sync")]
@@ -14,25 +12,159 @@ use serde::{de::DeserializeOwned, Deserialize};
 
 use crate::{bson::RawDocument, error::Result};
 
+/// A [`Cursor`] streams the result of a query. When a query is made, the returned [`Cursor`] will
+/// contain the first batch of results from the server; the individual results will then be returned
+/// as the [`Cursor`] is iterated. When the batch is exhausted and if there are more results, the
+/// [`Cursor`] will fetch the next batch of documents, and so forth until the results are exhausted.
+/// Note that because of this batching, additional network I/O may occur on any given call to
+/// `next`. Because of this, a [`Cursor`] iterates over `Result<T>` items rather than
+/// simply `T` items.
+///
+/// The batch size of the `Cursor` can be configured using the options to the method that returns
+/// it. For example, setting the `batch_size` field of
+/// [`FindOptions`](options/struct.FindOptions.html) will set the batch size of the
+/// `Cursor` returned by [`Collection::find`](struct.Collection.html#method.find).
+///
+/// Note that the batch size determines both the number of documents stored in memory by the
+/// `Cursor` at a given time as well as the total number of network round-trips needed to fetch all
+/// results from the server; both of these factors should be taken into account when choosing the
+/// optimal batch size.
+///
+/// [`Cursor`] implements [`Stream`](https://docs.rs/futures/latest/futures/stream/trait.Stream.html), which means
+/// it can be iterated over much in the same way that an `Iterator` can be in synchronous Rust. In
+/// order to do so, the [`StreamExt`](https://docs.rs/futures/latest/futures/stream/trait.StreamExt.html) trait must
+/// be imported. Because a [`Cursor`] iterates over a `Result<T>`, it also has access to the
+/// potentially more ergonomic functionality provided by
+/// [`TryStreamExt`](https://docs.rs/futures/latest/futures/stream/trait.TryStreamExt.html), which can be
+/// imported instead of or in addition to
+/// [`StreamExt`](https://docs.rs/futures/latest/futures/stream/trait.StreamExt.html). The methods from
+/// [`TryStreamExt`](https://docs.rs/futures/latest/futures/stream/trait.TryStreamExt.html) are especially useful when
+/// used in conjunction with the `?` operator.
+///
+/// ```rust
+/// # use mongodb::{bson::{Document, doc}, Client, error::Result};
+/// #
+/// # async fn do_stuff() -> Result<()> {
+/// # let client = Client::with_uri_str("mongodb://example.com").await?;
+/// # let coll = client.database("foo").collection::<Document>("bar");
+/// #
+/// use futures::stream::{StreamExt, TryStreamExt};
+///
+/// let mut cursor = coll.find(doc! {}).await?;
+/// // regular Stream uses next() and iterates over Option<Result<T>>
+/// while let Some(doc) = cursor.next().await {
+///   println!("{}", doc?)
+/// }
+/// // regular Stream uses collect() and collects into a Vec<Result<T>>
+/// let v: Vec<Result<_>> = cursor.collect().await;
+///
+/// let mut cursor = coll.find(doc! {}).await?;
+/// // TryStream uses try_next() and iterates over Result<Option<T>>
+/// while let Some(doc) = cursor.try_next().await? {
+///   println!("{}", doc)
+/// }
+/// // TryStream uses try_collect() and collects into a Result<Vec<T>>
+/// let v: Vec<_> = cursor.try_collect().await?;
+/// #
+/// # Ok(())
+/// # }
+/// ```
+///
+/// If a [`Cursor`] is still open when it goes out of scope, it will automatically be closed via an
+/// asynchronous [killCursors](https://www.mongodb.com/docs/manual/reference/command/killCursors/) command executed
+/// from its [`Drop`](https://doc.rust-lang.org/std/ops/trait.Drop.html) implementation.
 #[derive_where(Debug)]
 pub struct Cursor<T> {
     stream: common::Stream<'static, super::raw_batch::RawBatchCursor, T>,
 }
 
 impl<T> Cursor<T> {
+    /// Move the cursor forward, potentially triggering requests to the database for more results
+    /// if the local buffer has been exhausted.
+    ///
+    /// This will keep requesting data from the server until either the cursor is exhausted
+    /// or batch with results in it has been received.
+    ///
+    /// The return value indicates whether new results were successfully returned (true) or if
+    /// the cursor has been closed (false).
+    ///
+    /// Note: [`Cursor::current`] and [`Cursor::deserialize_current`] must only be called after
+    /// [`Cursor::advance`] returned `Ok(true)`. It is an error to call either of them without
+    /// calling [`Cursor::advance`] first or after [`Cursor::advance`] returns an error / false.
+    ///
+    /// ```
+    /// # use mongodb::{Client, bson::{Document, doc}, error::Result};
+    /// # async fn foo() -> Result<()> {
+    /// # let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+    /// # let coll = client.database("stuff").collection::<Document>("stuff");
+    /// let mut cursor = coll.find(doc! {}).await?;
+    /// while cursor.advance().await? {
+    ///     println!("{:?}", cursor.current());
+    /// }
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn advance(&mut self) -> Result<bool> {
         self.stream.state_mut().advance().await
     }
 
+    /// Returns a reference to the current result in the cursor.
+    ///
+    /// # Panics
+    /// [`Cursor::advance`] must return `Ok(true)` before [`Cursor::current`] can be
+    /// invoked. Calling [`Cursor::current`] after [`Cursor::advance`] does not return true
+    /// or without calling [`Cursor::advance`] at all may result in a panic.
+    ///
+    /// ```
+    /// # use mongodb::{Client, bson::{Document, doc}, error::Result};
+    /// # async fn foo() -> Result<()> {
+    /// # let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+    /// # let coll = client.database("stuff").collection::<Document>("stuff");
+    /// let mut cursor = coll.find(doc! {}).await?;
+    /// while cursor.advance().await? {
+    ///     println!("{:?}", cursor.current());
+    /// }
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn current(&self) -> &RawDocument {
         self.stream.state().current()
     }
 
+    /// Returns true if the cursor has any additional items to return and false otherwise.
     pub fn has_next(&self) -> bool {
         let state = self.stream.state();
         !state.is_empty() || state.raw.has_next()
     }
 
+    /// Deserialize the current result to the generic type associated with this cursor.
+    ///
+    /// # Panics
+    /// [`Cursor::advance`] must return `Ok(true)` before [`Cursor::deserialize_current`] can be
+    /// invoked. Calling [`Cursor::deserialize_current`] after [`Cursor::advance`] does not return
+    /// true or without calling [`Cursor::advance`] at all may result in a panic.
+    ///
+    /// ```
+    /// # use mongodb::{Client, error::Result, bson::doc};
+    /// # async fn foo() -> Result<()> {
+    /// # let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+    /// # let db = client.database("foo");
+    /// use serde::Deserialize;
+    ///
+    /// #[derive(Debug, Deserialize)]
+    /// struct Cat<'a> {
+    ///     #[serde(borrow)]
+    ///     name: &'a str
+    /// }
+    ///
+    /// let coll = db.collection::<Cat>("cat");
+    /// let mut cursor = coll.find(doc! {}).await?;
+    /// while cursor.advance().await? {
+    ///     println!("{:?}", cursor.deserialize_current()?);
+    /// }
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn deserialize_current<'a>(&'a self) -> Result<T>
     where
         T: Deserialize<'a>,
@@ -40,6 +172,7 @@ impl<T> Cursor<T> {
         self.stream.state().deserialize_current()
     }
 
+    /// Update the type streamed values will be parsed as.
     pub fn with_type<'a, D>(self) -> Cursor<D>
     where
         D: Deserialize<'a>,
@@ -49,6 +182,11 @@ impl<T> Cursor<T> {
         }
     }
 
+    /// Some tests need to be able to observe the events generated by `killCommand` execution;
+    /// however, because that happens asynchronously on `drop`, the test runner can conclude before
+    /// the event is published.  To fix that, tests can set a "kill watcher" on cursors - a
+    /// one-shot channel with a `()` value pushed after `killCommand` is run that the test can wait
+    /// on.
     #[cfg(test)]
     pub(crate) fn set_kill_watcher(&mut self, tx: tokio::sync::oneshot::Sender<()>) {
         self.stream.state_mut().raw.set_kill_watcher(tx);
