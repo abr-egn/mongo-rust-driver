fa3610a28f97dd0f33888738c7507794e846ea23
Abraham Egnor <abraham.egnor@mongodb.com>
2026-03-04 15:01:40 +0000

    insert_one stub

--- driver/src/ffi.rs
+++ driver/src/ffi.rs
@@ -14,41 +14,8 @@
 pub mod client;
 pub mod command;
 pub mod error;
+pub mod insert;
 mod runtime;
 pub mod session;
 pub mod types;
 mod utils;
-
-pub use client::{mongo_client_destroy, mongo_client_new, MongoClient};
-pub use command::{mongo_run_command, RunCommandCallback};
-pub use error::{error_free, Error};
-pub use session::{
-    mongo_session_abort_transaction,
-    mongo_session_commit_transaction,
-    mongo_session_end,
-    mongo_session_start,
-    mongo_session_start_transaction,
-    SessionOptions,
-    TransactionCallback,
-    TransactionOptions,
-};
-pub use types::{
-    mongo_read_concern_create,
-    mongo_read_concern_destroy,
-    mongo_read_preference_create,
-    mongo_read_preference_destroy,
-    mongo_write_concern_create,
-    mongo_write_concern_destroy,
-    AuthSettings,
-    Bson,
-    ClientSession,
-    ConnectionSettings,
-    OwnedBson,
-    ReadConcern,
-    ReadConcernOptions,
-    ReadPreference,
-    ReadPreferenceOptions,
-    TlsSettings,
-    WriteConcern,
-    WriteConcernOptions,
-};
